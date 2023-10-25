7c4bf0a1be9fb38a91e8fb30141381d2edcf84b6
Benttyman <jesustapiamartin@gmail.com>
2023-10-25 10:09:29 -0300

    Ventana Menú

--- src/main/java/Vista/fmrMenu.java
+++ src/main/java/Vista/fmrMenu.java
@@ -1,4 +1,10 @@
 package Vista;
 
+import javax.swing.*;
+
 public class fmrMenu {
+    private JButton btnAgregarAlumno;
+    private JPanel panel1;
+    private JButton btnInstitucion;
+    private JButton btnSalir;
 }
