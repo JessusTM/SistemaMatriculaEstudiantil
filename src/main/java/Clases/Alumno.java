76e32d908256f5c691791cbf9f8f68eb1d57de4e
Benttyman <jesustapiamartin@gmail.com>
2023-10-25 09:15:51 -0300

    Formato de Clases

--- src/main/java/Clases/Alumno.java
+++ src/main/java/Clases/Alumno.java
@@ -1,18 +1,21 @@
 package Clases;
 
 public class Alumno {
-    private String    rut                   , nombres               , apellidos
-            , edad                  , fechaDeNacimiento     , direccion
-            , ciudad                , telefono              , gestionadoPor
-            , email                 , viveCon               , nacionalidad
-            , sector                , datosAdicionales      , etnia
-            , matricula             , fechaDeMatricula      , insOrigen
-            , enfermedades          , medicamentos          , genero
-            , curso                 , letra                 , electivo
-            , discAuditiva          , discVisual            , discIntelectual
-            , tea;
 
-    // constructor: toma cada atributo y da un valor inicial predeterminado
+    // ========== ATRIBUTOS ==========
+    private String  rut                   , nombres               , apellidos,
+                    edad                  , fechaDeNacimiento     , direccion,
+                    ciudad                , telefono              , gestionadoPor,
+                    email                 , viveCon               , nacionalidad,
+                    sector                , datosAdicionales      , etnia,
+                    matricula             , fechaDeMatricula      , insOrigen,
+                    enfermedades          , medicamentos          , genero,
+                    curso                 , letra                 , electivo,
+                    discAuditiva          , discVisual            , discIntelectual,
+                    tea;
+
+
+    // ========== CONSTRUCTOR ==========
     public Alumno(  String rut              , String nombres                , String apellidos          ,
                     String edad             , String fechaDeNacimiento      , String direccion          ,
                     String ciudad           , String telefono               , String gestionadoPor      ,
@@ -23,10 +26,6 @@ public class Alumno {
                     String curso            , String letra                  , String electivo           ,
                     String discAuditiva     , String discVisual             , String discIntelectual    ,
                     String tea){
-
-        //this sirve para q el atributo de este objeto le damos este valor,
-        //se usa para referirse a los atributos de un objeto especifico de la clase
-
         this.rut                        = rut;
         this.nombres                    = nombres;
         this.apellidos                  = apellidos;
@@ -57,226 +56,290 @@ public class Alumno {
         this.tea                        = tea;
     }
 
-    //GETTERS Y SETTERS
+
+
+    // ========== CONSTRUCTOR VACÍO ==========
+    public Alumno() {
+    }
+
+
+
+
+    // ========== GETTERS Y SETTERS ==========
     public String getRut() {
         return rut;
     }
+
 
     public void setRut(String rut) {
         this.rut = rut;
     }
 
+
     public String getNombres() {
         return nombres;
     }
+
 
     public void setNombres(String nombres) {
         this.nombres = nombres;
     }
 
+
     public String getApellidos() {
         return apellidos;
     }
+
 
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+
     public String getEdad() {
         return edad;
     }
+
 
     public void setEdad(String edad) {
         this.edad = edad;
     }
 
+
     public String getFechaDeNacimiento() {
         return fechaDeNacimiento;
     }
+
 
     public void setFechaDeNacimiento(String fechaDeNacimiento) {
         this.fechaDeNacimiento = fechaDeNacimiento;
     }
 
+
     public String getDireccion() {
         return direccion;
     }
+
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+
     public String getCiudad() {
         return ciudad;
     }
+
 
     public void setCiudad(String ciudad) {
         this.ciudad = ciudad;
     }
 
+
     public String getTelefono() {
         return telefono;
     }
+
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
+
     public String getGestionadoPor() {
         return gestionadoPor;
     }
+
 
     public void setGestionadoPor(String gestionadoPor) {
         this.gestionadoPor = gestionadoPor;
     }
 
+
     public String getEmail() {
         return email;
     }
+
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+
     public String getViveCon() {
         return viveCon;
     }
+
 
     public void setViveCon(String viveCon) {
         this.viveCon = viveCon;
     }
 
+
     public String getNacionalidad() {
         return nacionalidad;
     }
+
 
     public void setNacionalidad(String nacionalidad) {
         this.nacionalidad = nacionalidad;
     }
 
+
     public String getSector() {
         return sector;
     }
+
 
     public void setSector(String sector) {
         this.sector = sector;
     }
 
+
     public String getDatosAdicionales() {
         return datosAdicionales;
     }
+
 
     public void setDatosAdicionales(String datosAdicionales) {
         this.datosAdicionales = datosAdicionales;
     }
 
+
     public String getEtnia() {
         return etnia;
     }
+
 
     public void setEtnia(String etnia) {
         this.etnia = etnia;
     }
 
+
     public String getMatricula() {
         return matricula;
     }
+
 
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
 
+
     public String getFechaDeMatricula() {
         return fechaDeMatricula;
     }
+
 
     public void setFechaDeMatricula(String fechaDeMatricula) {
         this.fechaDeMatricula = fechaDeMatricula;
     }
 
+
     public String getInsOrigen() {
         return insOrigen;
     }
+
 
     public void setInsOrigen(String insOrigen) {
         this.insOrigen = insOrigen;
     }
 
+
     public String getEnfermedades() {
         return enfermedades;
     }
+
 
     public void setEnfermedades(String enfermedades) {
         this.enfermedades = enfermedades;
     }
 
+
     public String getMedicamentos() {
         return medicamentos;
     }
+
 
     public void setMedicamentos(String medicamentos) {
         this.medicamentos = medicamentos;
     }
 
+
     public String getGenero() {
         return genero;
     }
+
 
     public void setGenero(String genero) {
         this.genero = genero;
     }
 
+
     public String getCurso() {
         return curso;
     }
+
 
     public void setCurso(String curso) {
         this.curso = curso;
     }
 
+
     public String getLetra() {
         return letra;
     }
+
 
     public void setLetra(String letra) {
         this.letra = letra;
     }
 
+
     public String getElectivo() {
         return electivo;
     }
+
 
     public void setElectivo(String electivo) {
         this.electivo = electivo;
     }
 
+
     public String getDiscAuditiva() {
         return discAuditiva;
     }
+
 
     public void setDiscAuditiva(String discAuditiva) {
         this.discAuditiva = discAuditiva;
     }
 
+
     public String getDiscVisual() {
         return discVisual;
     }
+
 
     public void setDiscVisual(String discVisual) {
         this.discVisual = discVisual;
     }
 
+
     public String getDiscIntelectual() {
         return discIntelectual;
     }
+
 
     public void setDiscIntelectual(String discIntelectual) {
         this.discIntelectual = discIntelectual;
     }
 
+
     public String getTea() {
         return tea;
     }
+
 
     public void setTea(String tea) {
         this.tea = tea;
--- src/main/java/Clases/Apoderado.java
+++ src/main/java/Clases/Apoderado.java
@@ -1,14 +1,17 @@
 package Clases;
 public class Apoderado {
-    String rut, nombre, apellidos,
-            parentesco, telefono, ciudad,
-            direccion, observaciones, genero;
+
+    // ========== ATRIBUTOS ==========
+    String  rut             , nombre            , apellidos     ,
+            parentesco      , telefono          , ciudad        ,
+            direccion       , observaciones     , genero;
 
 
-    public Apoderado(String rut,            String nombre,
-                     String apellidos,     String parentesco,
-                     String telefono,      String ciudad, String direccion,
-                     String observaciones, String genero ) {
+
+    // ========== CONSTRUCTOR ==========
+    public Apoderado(   String rut          , String nombre         , String apellidos      ,
+                        String parentesco   , String telefono       , String ciudad         ,
+                        String direccion    , String observaciones  , String genero ) {
 
         this.rut           = rut;
         this.nombre        = nombre;
@@ -19,83 +22,104 @@ public class Apoderado {
         this.direccion     = direccion;
         this.observaciones = observaciones;
         this.genero        = genero;
-
     }
+
+
+
+    // ========== CONSTRUCTOR VACÍO ==========
     public Apoderado() {
     }
 
 
+
+
+    // ========== GETTERS Y SETTERS ==========
     public String getRut() {
         return rut;
     }
+
 
     public void setRut(String rut) {
         this.rut = rut;
     }
 
+
     public String getNombre() {
         return nombre;
     }
+
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+
     public String getApellidos() {
         return apellidos;
     }
+
 
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+
     public String getParentesco() {
         return parentesco;
     }
+
 
     public void setParentesco(String parentesco) {
         this.parentesco = parentesco;
     }
 
+
     public String getTelefono() {
         return telefono;
     }
+
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
+
     public String getCiudad() {
         return ciudad;
     }
+
 
     public void setCiudad(String ciudad) {
         this.ciudad = ciudad;
     }
 
+
     public String getDireccion() {
         return direccion;
     }
+
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+
     public String getObservaciones() {
         return observaciones;
     }
+
 
     public void setObservaciones(String observaciones) {
         this.observaciones = observaciones;
     }
 
+
     public String getGenero() {
         return genero;
     }
 
+
     public void setGenero(String genero) {
         this.genero = genero;
     }
-
-
 }
