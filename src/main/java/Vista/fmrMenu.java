95855047b181579f9e508f7ada8b4747db9ae9ea
BenjaFA <b.fernandez07@ufromail.cl>
2023-10-26 01:02:51 -0300

    primer commit de la clase fmrMenu el cual contiene sus metodos correspondientes y el constructor

--- src/main/java/Vista/fmrMenu.java
+++ src/main/java/Vista/fmrMenu.java
@@ -1,10 +1,65 @@
 package Vista;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class fmrMenu {
+
+public class fmrMenu extends JFrame {
     private JButton btnAgregarAlumno;
-    private JPanel panel1;
     private JButton btnInstitucion;
-    private JButton btnSalir;
+    private JPanel Menu;
+    private JButton salirButton;
+
+
+    // ========== CONSTRUCTOR ==========
+    public fmrMenu(){
+
+        // ----- APARIENCIA WINDOWS -----
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        // ------------------------------
+
+
+
+
+        // TAMAÑO
+        setSize(1024, 768);
+        // POSICIÓN AL MEDIO
+        setLocationRelativeTo(null);
+        // TÍTULO VENTANA
+        setTitle("Menú");
+        // DISEÑO DE VENTANA DEL PANEL MENU
+        setContentPane(Menu);
+
+
+
+        // ----- BOTÓN CAMBIO DE VENTANA A AGREGAR ALUMNO -----
+        btnAgregarAlumno.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // OBJETO DE LA VENTANA QUE QUEREMOS ABRIR
+                fmrAgregarAlumnos ventana = new fmrAgregarAlumnos();
+                // SE MUESTRA
+                ventana.setVisible(true);
+                // TAMAÑO
+                ventana.setSize(400, 300);
+                // CENTRAMOS
+                ventana.setLocationRelativeTo(null);
+            }
+        });
+
+
+
+        // ----- BOTÓN TERMINAR PROGRAMA  -----
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
