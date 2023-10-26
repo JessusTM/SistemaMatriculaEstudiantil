3094b41e09cd4d0448cb41d30393edef0ac514c9
YoandriVO <y.villarroel01@ufromail.cl>
2023-10-26 01:19:34 -0300

    Correccion de la clase apoderado

--- src/main/java/Clases/Apoderado.java
+++ src/main/java/Clases/Apoderado.java
@@ -1,123 +1,102 @@
 package Clases;
+
 public class Apoderado {
 
-    // ========== ATRIBUTOS ==========
-    String  rut             , nombre            , apellidos     ,
-            parentesco      , telefono          , ciudad        ,
-            direccion       , observaciones     , genero;
+    // ==================== ATRIBUTOS ====================
+    String  rut         , nombres       , apellidos         , parentesco            ,
+            telefono    , ciudad        , direccion         , observaciones         ,
+            genero;
 
 
 
-    // ========== CONSTRUCTOR ==========
-    public Apoderado(   String rut          , String nombre         , String apellidos      ,
-                        String parentesco   , String telefono       , String ciudad         ,
-                        String direccion    , String observaciones  , String genero ) {
-
-        this.rut           = rut;
-        this.nombre        = nombre;
-        this.apellidos     = apellidos;
-        this.parentesco    = parentesco;
-        this.telefono      = telefono;
-        this.ciudad        = ciudad;
-        this.direccion     = direccion;
-        this.observaciones = observaciones;
-        this.genero        = genero;
+    //==================== CONSTRUCTORES ====================
+    public Apoderado(   String rut              , String nombres            , String apellidos              , String parentesco         ,
+                        String telefono         , String ciudad             , String direccion              , String observaciones      ,
+                        String genero) {
+        this.rut                = rut;
+        this.nombres            = nombres;
+        this.apellidos          = apellidos;
+        this.parentesco         = parentesco;
+        this.telefono           = telefono;
+        this.ciudad             = ciudad;
+        this.direccion          = direccion;
+        this.observaciones      = observaciones;
+        this.genero             = genero;
     }
 
 
 
-    // ========== CONSTRUCTOR VACÍO ==========
-    public Apoderado() {
-    }
-
-
-
-
-    // ========== GETTERS Y SETTERS ==========
+    //==================== GET ====================
     public String getRut() {
         return rut;
     }
 
-
-    public void setRut(String rut) {
-        this.rut = rut;
+    public String getNombres() {
+        return nombres;
     }
-
-
-    public String getNombre() {
-        return nombre;
-    }
-
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
 
     public String getApellidos() {
         return apellidos;
     }
 
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-
     public String getParentesco() {
         return parentesco;
     }
-
-
-    public void setParentesco(String parentesco) {
-        this.parentesco = parentesco;
-    }
-
 
     public String getTelefono() {
         return telefono;
     }
 
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-
     public String getCiudad() {
         return ciudad;
     }
-
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
-
 
     public String getDireccion() {
         return direccion;
     }
 
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-
     public String getObservaciones() {
         return observaciones;
     }
-
-
-    public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
-    }
-
 
     public String getGenero() {
         return genero;
     }
 
+
+
+    //==================== SET ====================
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setParentesco(String parentesco) {
+        this.parentesco = parentesco;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
 
     public void setGenero(String genero) {
         this.genero = genero;
