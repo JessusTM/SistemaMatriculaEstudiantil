cc2c9d9c6e234dbf95a9bf71fc5d41d2195262b0
JesusTapiaMartin <jesustapiamartin@gmail.com>
2023-10-26 01:46:09 -0300

    Package Launcher con el método main

---
+++ src/main/java/Launcher/Main.java
@@ -0,0 +1,12 @@
+package Launcher;
+
+import Vista.*;
+
+public class Main {
+
+    //==================== MAIN ====================
+    public static void main(String[] args) {
+        fmrMenu menu = new fmrMenu();
+        menu.setVisible(true);
+    }
+}
