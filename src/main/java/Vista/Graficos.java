746ce1891ffb29a3893411bec9b1f4f23117b96e
YoandriVO <y.villarroel01@ufromail.cl>
2023-12-10 00:27:31 -0300

    Añadida Clase y formulario "Graficos"

---
+++ src/main/java/Vista/Graficos.java
@@ -0,0 +1,13 @@
+package Vista;
+
+import javax.swing.*;
+
+public class Graficos extends JDialog {
+    private JPanel jpInicial;
+
+
+    public Graficos() {
+        setContentPane(jpInicial);
+    }
+
+}
