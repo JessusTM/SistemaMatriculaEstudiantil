0c9e5b63c95f3d6d9c364b15c504b06c30a42b3c
YoandriVO <y.villarroel01@ufromail.cl>
2023-12-10 01:20:23 -0300

    Creacion de la clase Graficos para mostrar informacion con graficos

--- src/main/java/Vista/Graficos.java
+++ src/main/java/Vista/Graficos.java
@@ -1,13 +1,71 @@
 package Vista;
 
+import Controlador.Controlador;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Graficos extends JDialog {
     private JPanel jpInicial;
+    private JLabel jbelTitulo;
+    private JButton btnGraficaGenero;
+    private JButton btnGraficoNose;
+    private JPanel jpGraficaGenero;
+    private JPanel jpGraficaNoSe;
+    private JButton btnVolver;
 
 
     public Graficos() {
+
         setContentPane(jpInicial);
+        setVisible(true);
+        setTitle("Estadisticas");
+        setLocationRelativeTo(null);
+        setModal(true);
+        setSize(1000,700);
+        setResizable(false);
+
+
+        btnGraficaGenero.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int contadorMasculino = Controlador.contarAlumnosMasculinos();
+                int contadorFemenino = Controlador.contarAlumnosFemeninos();
+                int contadorOtros = Controlador.contarAlumnosOtrosGeneros();
+
+                DefaultPieDataset datosContadores = new DefaultPieDataset();
+
+                datosContadores.setValue("Masculino",contadorMasculino);
+                datosContadores.setValue("Femenino",contadorFemenino);
+                datosContadores.setValue("Otro",contadorOtros);
+
+                JFreeChart grafico_circular = ChartFactory.createPieChart("Alumnos por genero",datosContadores,true,true,false);
+
+                ChartPanel panelGrafico = new ChartPanel(grafico_circular);
+                panelGrafico.setMouseWheelEnabled(true);
+                panelGrafico.setSize(300,200);
+
+                jpGraficaGenero.setLayout(new BorderLayout());
+                jpGraficaGenero.add(panelGrafico,BorderLayout.NORTH);
+                jpGraficaGenero.setSize(300,200);
+
+
+
+
+            }
+        });
+        btnVolver.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
     }
 
 }
